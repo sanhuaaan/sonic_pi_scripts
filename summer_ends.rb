2cb0337d3196167c50939f09861ad9a610467c00
sanhuaaan <jsanjuan83@gmail.com>
2023-09-10 13:13:24 +0200

    added sample and 32 step george

--- summer_ends.rb
+++ summer_ends.rb
@@ -13,12 +13,20 @@ defonce :novation do
     'george' => false
   }
 end
-
+################################################
+define :midi_chord do |notes, *args|
+  notes.each do |note|
+    midi note, *args,
+      port: 'midi_through_midi_through_port-0_14_0'
+  end
+end
+################################################
 
 live_loop :ringo do
   ##| stop
+  cue :ringo
   in_thread do
-    8.times do
+    16.times do
       tick
       density [1,1,1,1,1,1,2].choose do
         on spread(5,8).look do
@@ -28,36 +36,50 @@ live_loop :ringo do
         end
         sleep 0.5
       end
+      on spread(11,16).look do
+        if novation['ringo_6']
+          time_warp -0.25 do
+            sample folder + "Maracas [MA]/E808_MA-10.wav"
+          end
+        end
+      end
     end
   end
-  8.times do
+  16.times do
     tick
     if factor? look, 2
-      time_warp -0.2 do
-        ##| midi_clock_beat port: 'ch345_ch345_midi_1_24_0'
+      time_warp -0.15 do
+        midi_clock_beat #port: 'ch345_ch345_midi_1_24_0'
       end
     end
     
     on [1,0,1,0,1,0,1,0].ring.look do
       if novation['ringo_1']
-        sample folder + "Bass Drum [BD]/E808_BD[short]-03.wav", amp: 2
+        with_swing -0.25, pulse: 4 do
+          sample folder + "Bass Drum [BD]/E808_BD[short]-03.wav", amp: 2
+        end
       end
     end
     on [0,0,0,1,0,0,0,1].ring.look do
       if novation['ringo_2']
-        with_swing -0.25, pulse: 2 do
-          sample folder + "Snare Drum [SD]/E808_SD-02.wav", amp: 0.5
-        end
+        ##| with_swing -0.25, pulse: 2 do
+        sample folder + "Snare Drum [SD]/E808_SD-02.wav", amp: 0.4
+        ##| end
       end
     end
-    on [0,1,0,0].ring.look do
-      if novation['ringo_3']
+    if novation['ringo_3']
+      on [0,1,0,0,0,1,0,0].ring.look do
         sample folder + "Closed Hi Hat [CH]/E808_CH-01.wav"
       end
-    end
-    on [1,0,0,0,0,0,0,0].ring.look do
-      if novation['ringo_4']
+      on [1,0,0,0,0,0,0,0].ring.look do
         sample folder + "Open Hi Hat [OH]/E808_OH-03.wav", finish: 0.2, amp: 0.7
+      end
+    end
+    if novation['ringo_4']
+      on spread(5,16, rotate: 3).look do
+        with_swing -0.25, pulse: 4 do
+          sample folder + "Congas [HC-MC-LC]/E808_MC-08.wav", cutoff: 100
+        end
       end
     end
     sleep 0.5
@@ -65,48 +87,37 @@ live_loop :ringo do
 end
 
 live_loop :paul, sync: :ringo do
-  use_random_seed 555
-  8.times do
-    if novation['paul']
-      on spread(6,10).tick do
-        #Bass - CM Subsine
-        time_warp 0.25 do
-          midi octs(:b1, 4).choose,
-            port: 'midi_through_midi_through_port-0_14_0',
-            sustain: 0.2
-        end
-      end
-      cue :paul
-    end
-    sleep 0.5
+  if novation['paul']
+    #mod piano
+    ##| time_warp -0.15 do
+    midi_chord (chord_degree [1,3,5,2].ring.tick, :b2, :major, 3),
+      sustain: 3
+    ##| end
+    cue :paul
   end
+  sleep 4
 end
 
 live_loop :john, sync: :ringo do
-  stop
-  4.times do
-    i=[0,2,3,4].ring.tick
-    time_warp -0.15 do
-      midi scale(:b2, :major)[i],
-        port: 'ch345_ch345_midi_1_24_0',
-        sustain: 9
-    end
-    sleep 8
+  s = "/home/jsanjuan/musicradar-808-samples/manson_believe_me.wav"
+  if novation['john']
+    sample s
+    novation['john'] = false
   end
+  sleep sample_duration s
 end
 
 live_loop :george, sync: :ringo do
-  stop
-  use_random_seed 697
-  16.times do
-    with_swing -0.5 do
-      time_warp -0.25 do
-        midi chord(:b, :M, num_octaves: 2).choose,
-          port: 'nts-1_digital_kit_nts-1_digital_kit_nts-1_digital_28_0',
-          sustain: 1 if one_in(3)
+  use_random_seed 4
+  32.times do
+    if novation['george']
+      time_warp -0.15 do
+        midi (scale :b1, :mixolydian, num_octaves: 2).choose,
+          sustain: [0.2, 0.5].choose,
+          port: 'ch345_ch345_midi_1_24_0' unless one_in 5
       end
     end
-    sleep 1
+    sleep [0.25,0.5].choose
   end
 end
 
